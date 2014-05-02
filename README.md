d5a7eb7e3c2be6ec9a9566e6569b13e239b8cbd2
Thomas Cyron <thomas@thcyron.de>
2014-05-02 21:57:21 +0200

    Add Dijkstra’s algorithm

--- README.md
+++ README.md
@@ -10,10 +10,10 @@ Implemented
 * Graph data structure
 * Set data structure
 * [Kruskal’s algorithm](http://en.wikipedia.org/wiki/Kruskal%27s_algorithm)
+* [Dijkstra's algorithm](http://en.wikipedia.org/wiki/Dijkstra%27s_algorithm)
 
 Todo
 ----
 
 * [Breadth-first search](http://en.wikipedia.org/wiki/Breadth-first_search)
 * [Depth-first search](http://en.wikipedia.org/wiki/Depth-first_search)
-* [Dijkstra's algorithm](http://en.wikipedia.org/wiki/Djikstra%27s_Algorithm)
---
+++ dijkstra.go
@@ -0,0 +1,97 @@
+package graphs
+
+import (
+	"container/heap"
+	"container/list"
+	"math"
+)
+
+type djikstraNode struct {
+	vertex      Vertex
+	distance    float64
+	predecessor *djikstraNode
+	index       int // Index of the node in the heap
+}
+
+type djikstraPQ []*djikstraNode
+
+func (dpq djikstraPQ) Len() int {
+	return len(dpq)
+}
+
+func (dpq djikstraPQ) Less(i, j int) bool {
+	return dpq[i].distance < dpq[j].distance
+}
+
+func (dpq djikstraPQ) Swap(i, j int) {
+	dpq[i], dpq[j] = dpq[j], dpq[i]
+	dpq[i].index, dpq[j].index = i, j
+}
+
+func (dpq *djikstraPQ) Push(x interface{}) {
+	dn := x.(*djikstraNode)
+	dn.index = len(*dpq)
+	*dpq = append(*dpq, dn)
+}
+
+func (dpq *djikstraPQ) Pop() interface{} {
+	n := len(*dpq)
+	node := (*dpq)[n-1]
+	*dpq = (*dpq)[0 : n-1]
+	return node
+}
+
+func Dijkstra(g *Graph, start, end Vertex) *list.List {
+	pq := djikstraPQ{}
+	nodes := map[Vertex]*djikstraNode{}
+
+	heap.Init(&pq)
+
+	for v, _ := range *g.Vertices {
+		dn := &djikstraNode{
+			vertex:   v,
+			distance: math.Inf(1),
+		}
+		heap.Push(&pq, dn)
+		nodes[v] = dn
+	}
+
+	nodes[start].distance = 0
+	heap.Fix(&pq, nodes[start].index)
+
+	for pq.Len() > 0 {
+		v := heap.Pop(&pq).(*djikstraNode)
+
+		for vv, _ := range *g.Adjacency[v.vertex] {
+			he := vv.(Halfedge)
+			dn := nodes[he.End]
+
+			if dn == nil {
+				continue
+			}
+
+			if math.IsInf(dn.distance, 1) {
+				dn.distance = he.Cost
+				dn.predecessor = v
+				heap.Fix(&pq, dn.index)
+			} else {
+				newCost := dn.distance + he.Cost
+				if newCost < dn.distance {
+					dn.distance = newCost
+					dn.predecessor = v
+					heap.Fix(&pq, dn.index)
+				}
+			}
+		}
+
+		if v.vertex == end {
+			l := list.New()
+			for e := v; e != nil; e = e.predecessor {
+				l.PushFront(e.vertex)
+			}
+			return l
+		}
+	}
+
+	return nil
+}
