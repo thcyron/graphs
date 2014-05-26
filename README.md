14c4a4f4ddb5ac4906d2d2a3b5e8fc2b93092495
Thomas Cyron <thomas@thcyron.de>
2014-05-26 15:34:19 +0200

    Implement Floyd–Warshall algorithm

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ Implemented
 * [Tarjan’s strongly connected components algorithm](http://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm)
 * [Bellman-Ford algorithm](http://en.wikipedia.org/wiki/Bellman%E2%80%93Ford_algorithm)
 * [Prim’s algorithm](http://en.wikipedia.org/wiki/Prim%27s_algorithm)
+* [Floyd–Warshall algorithm](http://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm)
 
 Todo
 ----
---
+++ floyd_warshall_test.go
@@ -0,0 +1,31 @@
+package graphs
+
+import (
+	"testing"
+)
+
+func TestFloydWarshall(t *testing.T) {
+	graph := NewDigraph()
+
+	graph.AddEdge("a", "b", 1)
+	graph.AddEdge("a", "c", 3)
+	graph.AddEdge("b", "g", 5)
+	graph.AddEdge("c", "g", 8)
+	graph.AddEdge("g", "h", 6)
+	graph.AddEdge("c", "d", 2)
+	graph.AddEdge("g", "f", 4)
+	graph.AddEdge("d", "f", 3)
+	graph.AddEdge("d", "e", 5)
+
+	m := FloydWarshall(graph)
+
+	for v, mm := range m {
+		for w, c := range mm {
+			t.Logf("m[%v][%v] = %f", v, w, c)
+		}
+	}
+
+	if c := m["a"]["e"]; c != 10 {
+		t.Errorf("bad shortest cost %f for a-e", c)
+	}
+}
